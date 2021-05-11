14aeda61d98015997ec66868846e179896f333a1
RohithYelagam <rohithyalagam2001@gmail.com>
2021-05-11 18:16:52 +0530

    ss

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://arena-backend-9.herokuapp.com',
   });
 
 export default instance;
